cf05bd47920852023c42bb9ac71e7fa43b53a608
Alex Williams <implementnap@gmail.com>
2016-08-15 12:45:40 -0500

    Added FTP.CopyFromServer.

---
+++ Application/Network/FTP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Network
+{
+    public static class FTP
+    {
+        public static void CopyFromServer(string ftpServerName, string userName, string passWord, string ftpFilePath, string destinationPath)
+        {
+            using (var request = new WebClient())
+            {
+                request.Credentials = new NetworkCredential(userName, passWord);
+                var fileData = request.DownloadData("ftp://" + ftpServerName + ftpFilePath);
+
+                using (var file = File.Create(destinationPath))
+                {
+                    file.Write(fileData, 0, fileData.Length);
+                    file.Close();
+                }
+            }
+        }
+    }
+}
